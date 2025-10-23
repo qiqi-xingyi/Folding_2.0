a873d659099cff4a9ae65258d411c4f87703c624
yuqizhang <yuqizhang247@gmail.com>
2025-10-22 21:15:21 -0400

    Bug fix

--- qsadpp/__init__.py
+++ qsadpp/__init__.py
@@ -4,12 +4,16 @@
 # @Email : yzhan135@kent.edu
 # @File:__init__.py.py
 
+# --*-- coding:utf-8 --*--
 # qsadpp/__init__.py
-"""
-Public exports for qsadpp.
-"""
 
-from .data import get_mj_table
+from .io import (
+    load_all_samples,
+    load_mj_matrix,
+    normalize_counts_to_prob_within,
+    expand_bitstrings,
+    ensure_standard_columns,
+)
 from .features import (
     compute_tierA_features,
     compute_features_for_group,
@@ -18,23 +22,28 @@ from .features import (
     radius_of_gyration,
 )
 from .decoder import ProteinShapeDecoder
-from .reverse_decoder import ReverseDecoder, batch_decode_vectors, batch_decode_coords_via_problem
+from .reverse_decoder import (
+    ReverseDecoder,
+    batch_decode_vectors,
+    batch_decode_coords_via_problem,
+)
 from .cluster import ClusterConfig, cluster_group, select_topK_per_group
+from .data import get_mj_table
 from .utils import write_xyz_ca
 
 __all__ = [
-    "get_mj_table",
-    "compute_tierA_features",
-    "compute_features_for_group",
-    "TierAWeights",
-    "pairwise_distances",
-    "radius_of_gyration",
-    "ProteinShapeDecoder",
-    "ReverseDecoder",
-    "batch_decode_vectors",
-    "batch_decode_coords_via_problem",
-    "ClusterConfig",
-    "cluster_group",
-    "select_topK_per_group",
-    "write_xyz_ca",
+    # io
+    "load_all_samples", "load_mj_matrix", "normalize_counts_to_prob_within",
+    "expand_bitstrings", "ensure_standard_columns",
+    # features
+    "compute_tierA_features", "compute_features_for_group", "TierAWeights",
+    "pairwise_distances", "radius_of_gyration",
+    # decoder & reverse
+    "ProteinShapeDecoder", "ReverseDecoder",
+    "batch_decode_vectors", "batch_decode_coords_via_problem",
+    # cluster
+    "ClusterConfig", "cluster_group", "select_topK_per_group",
+    # data/utils
+    "get_mj_table", "write_xyz_ca",
 ]
+
