40a78d94efe859f6d318bb6d2eacbedf34b1394f
yuqizhang <yuqizhang247@gmail.com>
2025-10-30 18:17:13 -0400

    Feature

--- post_process.py
+++ post_process.py
@@ -6,21 +6,28 @@
 
 from qsadpp.orchestrator import OrchestratorConfig, PipelineOrchestrator
 from qsadpp.io_reader import ReaderOptions
+from qsadpp.feature_calculator import FeatureConfig
 
 cfg = OrchestratorConfig(
     pdb_dir="./quantum_data/1m7y",
     reader_options=ReaderOptions(
-        chunksize=100000,
+        chunksize=100_000,
         strict=True,
         categorize_strings=True,
         include_all_csv=False,
     ),
     fifth_bit=False,
     out_dir="e_results",
-    decoder_output_format="jsonl",
-    aggregate_only=True,
-)
 
+    # ===== NEW: turn on features =====
+    compute_features=True,
+    feature_from="decoded",
+    combined_feature_name="features.jsonl",
+    feature_config=FeatureConfig(
+        output_format="jsonl",
+
+    ),
+)
 
 if __name__ == "__main__":
     runner = PipelineOrchestrator(cfg)
