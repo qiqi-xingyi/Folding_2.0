82dce4e53a384a93c3109c166543af69aa852734
yuqizhang <yuqizhang247@gmail.com>
2025-11-03 23:18:56 -0500

    Count data

--- Ghost_RMSD/training_dataset/count_data.py
+++ Ghost_RMSD/training_dataset/count_data.py
@@ -5,16 +5,16 @@
 # @File:count_data.py
 
 
-import sys
 
 def count_lines(file_path):
     with open(file_path, 'r', encoding='utf-8') as f:
         return sum(1 for _ in f)
 
 if __name__ == "__main__":
-    if len(sys.argv) < 2:
-        print("Usage: python count_lines.py <file.jsonl>")
-    else:
-        path = 'train.jsonl'
-        num_lines = count_lines(path)
-        print(f"Total lines: {num_lines}")
+
+    file_path = "train.jsonl"
+
+    num_lines = count_lines(file_path)
+    print(f"File: {file_path}")
+    print(f"Total lines: {num_lines}")
+
